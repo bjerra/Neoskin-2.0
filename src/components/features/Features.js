5b8fd8bfae95b0a5659b0f21c262cc0ecd26a019
bjerra <43780987+bjerra@users.noreply.github.com>
2021-10-26 21:54:29 +0200

    added some stuff

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {StyledFeatures, FeatureCard, Image} from './Features.styled'
+import {StyledFeatures} from './Features.styled'
 
 
 
