468f4a0424747a27e28da8fc54d5e4577b13e0b3
bjerra <43780987+bjerra@users.noreply.github.com>
2021-10-26 21:28:16 +0200

    added some stuff

--- src/components/features/Features.js
+++ src/components/features/Features.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {StyledFeatures, FeatureCard, Image} from './Features.styled'
-import SwiperCore, { Navigation, Pagination, A11y, Autoplay, EffectCoverflow } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-import 'swiper/swiper-bundle.min.css';
-
-SwiperCore.use([Navigation, Pagination, A11y, Autoplay, EffectCoverflow]);
 
 
 const FeatureGrid = ({ services }) => {
@@ -15,35 +9,7 @@ const FeatureGrid = ({ services }) => {
   return(
   <StyledFeatures>
     
-  <Swiper
-      spaceBetween={0}
-      slidesPerView={1}
-      loop={true}
-      autoplay= {{
-        delay: 3500,
-        disableOnInteraction: true,
-      }}
-      pagination={{ clickable: true }}
-     
-     
-    >
-       {[].map(service => (
-          <SwiperSlide key={service.title} >
-            <FeatureCard to={`/behandlingar/${service.slug}`} state={{modal: true}}>
-               
-         
-              <Image>
-                <GatsbyImage image={getImage(service.image)} alt={service.category} />
-                <h3>{service.title}</h3>
-              </Image>
-             
-              <button>Läs Mer</button>
-            </FeatureCard>
-          </SwiperSlide>
-        ))}
- 
-    </Swiper>
-    
+
   </StyledFeatures>
 )}
 
