8a669646b8b30d7c2476dba1da70e9456705d9bc
bjerra <43780987+bjerra@users.noreply.github.com>
2021-10-26 22:32:26 +0200

    added some stuff

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,7 @@
 export {default as Banner} from "./banner/Banner";
 export {default as Navbar} from "./navbar/Navbar";
 export {default as Layout} from "./layout";
-export {default as Features} from "./features";
 export {default as Divider} from "./divider/Divider";
-export {default as ServiceGrid} from "./serviceGrid/Servicegrid";
 export {default as Logo} from "./logo/Logo";
-export {default as Video} from "./video/Video";
 export {default as BokaButton} from "./buttons/BokaButton";
 export {default as ExpandIcon} from "./expandIcon/ExpandIcon";
--- src/components/video/Video.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "@emotion/styled"
-
-export const StyledVideo = styled.div`
-    width: 100%;
-    height: 100%;
-}
-`
