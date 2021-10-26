64dc6b56e2e65855dd808014492caf2a8da4f5b2
bjerra <43780987+bjerra@users.noreply.github.com>
2021-10-26 15:16:00 +0200

    added some stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,6 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-sitemap`,
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
       resolve: 'gatsby-source-filesystem',
